7525eae2719e6ce5db0a187fd6d7abd20d0d22c4
Maksim Odnoletkov <odnoletkov@mail.ru>
2021-05-18 14:59:10 +0100

    Decode

--- Sources/urlcomponents/main.swift
+++ Sources/urlcomponents/main.swift
@@ -1,1 +1,30 @@
-print("Hello, world!")
+import Foundation
+
+precondition(ProcessInfo.processInfo.arguments.count == 2)
+
+let components = URLComponents(string: ProcessInfo.processInfo.arguments[1])!
+
+extension Encodable {
+    func jsonObject() throws -> Any {
+        try JSONSerialization.jsonObject(with: try JSONEncoder().encode(self))
+    }
+}
+
+var json = try components.jsonObject() as! [String: Any]
+
+extension URLQueryItem: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        enum Keys: CodingKey {
+            case name
+            case value
+        }
+        var container = encoder.container(keyedBy: Keys.self)
+        try container.encode(value, forKey: .value)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+json["query"] = try components.queryItems?.jsonObject()
+
+let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+print(String(data: data, encoding: .utf8)!)
