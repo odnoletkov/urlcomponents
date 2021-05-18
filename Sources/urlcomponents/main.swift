adfd9df72081f97b1e5c5e702b1ab7ea56e2d76e
Maksim Odnoletkov <odnoletkov@mail.ru>
2021-05-18 19:31:34 +0100

    Setup encode mode with zero args

--- Sources/urlcomponents/main.swift
+++ Sources/urlcomponents/main.swift
@@ -1,25 +1,27 @@
 import Foundation
 
-precondition(ProcessInfo.processInfo.arguments.count == 2)
+switch ProcessInfo.processInfo.arguments.count {
 
-let components = URLComponents(string: ProcessInfo.processInfo.arguments[1])!
+case 1:
+    fatalError()
 
-extension Encodable {
-    func jsonObject() throws -> Any {
-        try JSONSerialization.jsonObject(with: try JSONEncoder().encode(self))
+case 2:
+    let components = URLComponents(string: ProcessInfo.processInfo.arguments[1])!
+
+    var json = try JSONSerialization.jsonObject(with: try JSONEncoder().encode(components)) as! [String: Any]
+
+    json["query"] = components.queryItems.map {
+        Dictionary($0.map { ($0.name, $0.value) }) { $1 }
     }
+
+    var options = JSONSerialization.WritingOptions.prettyPrinted
+    if #available(OSX 10.13, *) {
+        options.formUnion(.sortedKeys)
+    }
+
+    let data = try JSONSerialization.data(withJSONObject: json, options: options)
+    print(String(data: data, encoding: .utf8)!)
+
+default:
+    fatalError("expected zero or one argument")
 }
-
-var json = try components.jsonObject() as! [String: Any]
-
-json["query"] = components.queryItems.map {
-    Dictionary($0.map { ($0.name, $0.value) }) { $1 }
-}
-
-var options = JSONSerialization.WritingOptions.prettyPrinted
-if #available(OSX 10.13, *) {
-    options.formUnion(.sortedKeys)
-}
-
-let data = try JSONSerialization.data(withJSONObject: json, options: options)
-print(String(data: data, encoding: .utf8)!)
